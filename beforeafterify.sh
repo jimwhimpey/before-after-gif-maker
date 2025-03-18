134242996f0e147f158d2fa70ad17ee9f0bf910e
Jim Whimpey <jwhimpey@slack-corp.com>
2025-03-18 12:48:18 -0700

    Make some very specific changes for myself

--- beforeafterify.sh
+++ beforeafterify.sh
@@ -8,16 +8,16 @@ while [[ "$#" > 1 ]]; do case $1 in
   esac; shift; shift
 done
 
-convert ${before:-before.png} \
+magick ${before:-before.png} \
 	-colorspace sRGB \
-	-fill 'rgba(0,0,0,0.8)' -draw 'rectangle 10,10,100,42' \
-	-fill 'rgba(255,255,255, 0.7)' -font CourierNew -pointsize 20 -annotate +20+32 'Before' \
+	-fill 'rgba(0,0,0,0.8)' -draw 'rectangle 10,10,160,62' \
+	-fill 'rgba(255,255,255, 0.7)' -font /Users/jwhimpey/Library/Fonts/OperatorMono-BoldItalic.otf -pointsize 38 -annotate +21+47 'Before' \
 	labelled-before.gif \
 
-convert ${after:-after.png} \
+magick ${after:-after.png} \
 	-colorspace sRGB \
-	-fill 'rgba(0,0,0,0.8)' -draw 'rectangle 10,10,90,42' \
-	-fill 'rgba(255,255,255, 0.7)' -font CourierNew -pointsize 20 -annotate +20+32 'After' \
+	-fill 'rgba(0,0,0,0.8)' -draw 'rectangle 10,10,160,62' \
+	-fill 'rgba(255,255,255, 0.7)' -font /Users/jwhimpey/Library/Fonts/OperatorMono-BoldItalic.otf -pointsize 38 -annotate +32+47 'After' \
 	labelled-after.gif \
 
 gifsicle --delay=150 --loop --colors 256 --merge labelled-before.gif labelled-after.gif -o ${output:-beforeafter}.gif \
